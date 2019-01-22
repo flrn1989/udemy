547a799d26d344ea3467764c0d6f935f493d384c
Francisco Lopes <flrn1989@gmail.com>
2019-01-22 19:22:14 -0200

    Criação do projeto.

---
+++ modulo_01/src/main/java/udemy/curso/Modulo01Application.java
@@ -0,0 +1,14 @@
+package udemy.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo01Application.class, args);
+	}
+
+}
+
---
+++ modulo_01/src/test/java/udemy/curso/Modulo01ApplicationTests.java
@@ -0,0 +1,17 @@
+package udemy.curso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Modulo01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
