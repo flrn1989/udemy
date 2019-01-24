0de2e9c886b985c8d2bee942c9e4b4b0c874e2e5
Francisco Lopes <flrn1989@gmail.com>
2019-01-24 18:24:04 -0200

    Mapeamento de entidade testado. Inclusão do H2.

--- modulo_01/src/main/java/udemy/curso/categoria/Categoria.java
+++ modulo_01/src/main/java/udemy/curso/categoria/Categoria.java
@@ -5,10 +5,18 @@ package udemy.curso.categoria;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /** Classe de domínio de Categoria. */
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 
