4c15b76987378ee195669f5ae4c93219e0d11075
Francisco Lopes <flrn1989@gmail.com>
2020-02-25 16:49:03 -0300

    Limpeza com lombok.

--- modulo_01/src/main/java/udemy/curso/dominios/embeddedids/ProdutoPedidoId.java
+++ modulo_01/src/main/java/udemy/curso/dominios/embeddedids/ProdutoPedidoId.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package udemy.curso.dominios.embeddedids;
 
 import java.io.Serializable;
@@ -11,11 +8,16 @@ import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import udemy.curso.dominios.Pedido;
 import udemy.curso.dominios.Produto;
 
-/** Id para a ProdutoPedido. */
 @Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public final class ProdutoPedidoId implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -28,76 +30,5 @@ public final class ProdutoPedidoId implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "PEDIDO_ID")
 	private Pedido pedido;
-
-	/** Construtor padrão. */
-	public ProdutoPedidoId() {
-	}
-
-	/** @param produto
-	 * @param pedido */
-	public ProdutoPedidoId(Produto produto, Pedido pedido) {
-		this.produto = produto;
-		this.pedido = pedido;
-	}
-
-	/** {@inheritDoc} */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((pedido == null) ? 0 : pedido.hashCode());
-		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
-		return result;
-	}
-
-	/** {@inheritDoc} */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		ProdutoPedidoId other = (ProdutoPedidoId) obj;
-		if (pedido == null) {
-			if (other.pedido != null) {
-				return false;
-			}
-		} else if (!pedido.equals(other.pedido)) {
-			return false;
-		}
-		if (produto == null) {
-			if (other.produto != null) {
-				return false;
-			}
-		} else if (!produto.equals(other.produto)) {
-			return false;
-		}
-		return true;
-	}
-
-	/** @return the produto */
-	public Produto getProduto() {
-		return produto;
-	}
-
-	/** @param produto the produto to set */
-	public void setProduto(Produto produto) {
-		this.produto = produto;
-	}
-
-	/** @return the pedido */
-	public Pedido getPedido() {
-		return pedido;
-	}
-
-	/** @param pedido the pedido to set */
-	public void setPedido(Pedido pedido) {
-		this.pedido = pedido;
-	}
 
 }
