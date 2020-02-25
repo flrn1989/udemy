34f1644c8a24cfaccbe5d656796b00bbeb93ee33
Francisco Lopes <flrn1989@gmail.com>
2020-02-25 16:00:29 -0300

    Remoção de comentários.

--- modulo_01/src/main/java/udemy/curso/controladores/ControladorDeCategoria.java
+++ modulo_01/src/main/java/udemy/curso/controladores/ControladorDeCategoria.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package udemy.curso.controladores;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 import udemy.curso.dominios.Categoria;
 import udemy.curso.dto.CategoriaDTO;
 
-/** Controlador do domínio Categoria. */
 @RestController
 @RequestMapping(value = "/categorias")
 public class ControladorDeCategoria extends ControladorDeDominio<Categoria, CategoriaDTO> {
