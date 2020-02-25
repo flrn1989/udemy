62b29f1876cfa16cee5ecf16dcddc124a27a755e
Francisco Lopes <flrn1989@gmail.com>
2020-02-25 16:51:58 -0300

    Limpeza com lombok.

--- modulo_01/src/main/java/udemy/curso/dominios/enums/TipoCliente.java
+++ modulo_01/src/main/java/udemy/curso/dominios/enums/TipoCliente.java
@@ -1,33 +1,18 @@
-/**
- * 
- */
 package udemy.curso.dominios.enums;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import udemy.curso.interfaces.EnumIdentificavel;
 
-/** Enumerador para TipoCliente. */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum TipoCliente implements EnumIdentificavel {
 
 	PF(1, "Pessoa Física"),
 	PJ(2, "Pessoa Jurídica");
 
 	private Integer id;
-
 	private String descricao;
-
-	private TipoCliente(Integer id, String descricao) {
-		this.id = id;
-		this.descricao = descricao;
-	}
-
-	/** {@inheritDoc} */
-	public Integer getId() {
-		return id;
-	}
-
-	/** {@inheritDoc} */
-	public String getDescricao() {
-		return descricao;
-	}
 
 }
