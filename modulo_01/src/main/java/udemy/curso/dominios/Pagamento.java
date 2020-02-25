560664d4a8da1fddd57c91179f7930ad2b5673b2
Francisco Lopes <flrn1989@gmail.com>
2020-02-25 16:58:04 -0300

    Limpeza com lombok.

--- modulo_01/src/main/java/udemy/curso/dominios/Pagamento.java
+++ modulo_01/src/main/java/udemy/curso/dominios/Pagamento.java
@@ -1,7 +1,6 @@
-/**
- * 
- */
 package udemy.curso.dominios;
+
+import java.util.Objects;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -15,13 +14,18 @@ import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import udemy.curso.dominios.enums.EstadoDePagamento;
 import udemy.curso.interfaces.Dominio;
 import udemy.curso.util.ExtratoraDeEnum;
 
-/** Domínio de Pagamento. */
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@NoArgsConstructor
+@EqualsAndHashCode(of = { "id" })
+@Data
 public abstract class Pagamento implements Dominio {
 
 	private static final long serialVersionUID = 1L;
@@ -38,83 +42,22 @@ public abstract class Pagamento implements Dominio {
 	@MapsId
 	private Pedido pedido;
 
-	/** Construtor padrão. */
-	public Pagamento() {
-	}
-
-	/** @param estadoPagamento
-	 * @param pedido */
 	public Pagamento(EstadoDePagamento estadoPagamento, Pedido pedido) {
 		this.idDoEstadoPagamento = estadoPagamento.getId();
 		this.pedido = pedido;
-
-		if (this.pedido != null) {
+		if (Objects.nonNull(this.pedido)) {
 			this.pedido.setPagamento(this);
 		}
 	}
 
-	/** {@inheritDoc} */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	/** {@inheritDoc} */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Pagamento other = (Pagamento) obj;
-		if (id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!id.equals(other.id)) {
-			return false;
-		}
-		return true;
-	}
-
-	/** @return the id */
-	public Integer getId() {
-		return id;
-	}
-
-	/** @param id the id to set */
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	/** @return the EstadoDePagamento */
 	public EstadoDePagamento getIdDoEstadoPagamento() {
 		return (EstadoDePagamento) ExtratoraDeEnum
 				.extrairDe(EstadoDePagamento.values())
 				.valorCom(idDoEstadoPagamento);
 	}
 
-	/** @param estadoDePagamento the EstadoDePagamento with the id to set */
 	public void setIdDoEstadoPagamento(EstadoDePagamento estadoDePagamento) {
 		this.idDoEstadoPagamento = estadoDePagamento.getId();
-	}
-
-	/** @return the pedido */
-	public Pedido getPedido() {
-		return pedido;
-	}
-
-	/** @param pedido the pedido to set */
-	public void setPedido(Pedido pedido) {
-		this.pedido = pedido;
 	}
 
 }
