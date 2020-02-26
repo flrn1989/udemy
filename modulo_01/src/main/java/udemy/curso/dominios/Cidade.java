90b9cafeabe990f51e78f14f7359c8c0aac90d37
Francisco Lopes <flrn1989@gmail.com>
2020-02-26 14:30:19 -0300

    Refactoring com lombok.

--- modulo_01/src/main/java/udemy/curso/dominios/Cidade.java
+++ modulo_01/src/main/java/udemy/curso/dominios/Cidade.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package udemy.curso.dominios;
 
 import java.util.HashSet;
@@ -16,11 +13,17 @@ import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import udemy.curso.dto.CidadeDTO;
 import udemy.curso.interfaces.DTO;
 import udemy.curso.interfaces.Dominio;
 
-/** Domínio de Cidade. */
 @Entity
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(of = { "nome", "estado" })
 public class Cidade implements Dominio {
 
 	private static final long serialVersionUID = 1L;
@@ -39,108 +42,19 @@ public class Cidade implements Dominio {
 	@Transient
 	private Set<Endereco> enderecos = new HashSet<>();
 
-	/** Construtor padrão. */
-	public Cidade() {
-	}
-
-	/** @param nome
-	 * @param estado */
 	public Cidade(String nome, Estado estado) {
 		this.nome = nome;
 		this.estado = estado;
 		estado.getCidades().add(this);
 	}
 
-	/** @param id */
 	public Cidade(Integer id) {
 		this.id = id;
 	}
 
-	/** {@inheritDoc} */
 	@Override
 	public DTO<Cidade> paraDTO() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/** {@inheritDoc} */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((estado == null) ? 0 : estado.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		return result;
-	}
-
-	/** {@inheritDoc} */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Cidade other = (Cidade) obj;
-		if (estado == null) {
-			if (other.estado != null) {
-				return false;
-			}
-		} else if (!estado.equals(other.estado)) {
-			return false;
-		}
-		if (nome == null) {
-			if (other.nome != null) {
-				return false;
-			}
-		} else if (!nome.equals(other.nome)) {
-			return false;
-		}
-		return true;
-	}
-
-	/** @return the id */
-	public Integer getId() {
-		return id;
-	}
-
-	/** @param id the id to set */
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	/** @return the nome */
-	public String getNome() {
-		return nome;
-	}
-
-	/** @param nome the nome to set */
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	/** @return the estado */
-	public Estado getEstado() {
-		return estado;
-	}
-
-	/** @param estado the estado to set */
-	public void setEstado(Estado estado) {
-		this.estado = estado;
-	}
-
-	/** @return the enderecos */
-	public Set<Endereco> getEnderecos() {
-		return enderecos;
-	}
-
-	/** @param enderecos the enderecos to set */
-	public void setEnderecos(Set<Endereco> enderecos) {
-		this.enderecos = enderecos;
+		return new CidadeDTO(nome, estado);
 	}
 
 }
